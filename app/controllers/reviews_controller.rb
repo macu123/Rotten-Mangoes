114ebb15e3fc313dadf7b6dfd26287108bcbc030
John <chenjiapeng12345@gmail.com>
2015-09-23 03:04:29 +0000

    restricting access to create reviews if not login

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_filter :load_movie
+  before_filter :restrict_access, :load_movie
 
   def new
     @review = @movie.reviews.build
@@ -17,6 +17,13 @@ class ReviewsController < ApplicationController
   end
 
   protected
+
+  def restrict_access
+    if !current_user
+      flash[:alert] = "You must login in."
+      redirect_to new_session_path
+    end
+  end
 
   def load_movie
     @movie = Movie.find(params[:movie_id])
