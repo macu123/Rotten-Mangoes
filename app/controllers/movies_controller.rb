0d92d1bf54b2c95d9ed1489a09245d760a651411
John <chenjiapeng12345@gmail.com>
2015-09-23 17:33:47 +0000

    upload movie poster

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -44,6 +44,6 @@ class MoviesController < ApplicationController
   protected
 
   def movie_params
-    params.require(:movie).permit(:title, :release_date, :director, :runtime_in_minutes, :poster_image_url, :description)
+    params.require(:movie).permit(:title, :release_date, :director, :runtime_in_minutes, :poster_image_url, :image, :description)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,8 @@
 class Movie < ActiveRecord::Base
   has_many :reviews
+  mount_uploader :image, ImageUploader
 
-  validates :title, :director, :description, :poster_image_url, :release_date, presence: true
+  validates :title, :director, :description, :release_date, presence: true
   validates :runtime_in_minutes, numericality: {only_integer: true}
   validate :release_date_is_in_the_future
 
